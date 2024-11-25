aaaa3d4174e1dec96dfd84726b93a30637438a04
Meganselvidge <meganselvidge@gmail.com>
2024-11-25 10:37:19 +0100

    refactor: Repository redo att skrivas

--- HenriksHobbyLager/Models/Repository.cs
+++ HenriksHobbyLager/Models/Repository.cs
@@ -1,6 +1,36 @@
-﻿namespace RefactoringExercise.Models;
+﻿using RefactoringExercise.Repositories;
+using Microsoft.EntityFrameworkCore.Sqlite;
+namespace RefactoringExercise.Models;
 
-public class Repository
+public class Repository<T>: IRepository<T>
 {
-    
+    public IEnumerable<T> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public T GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Add(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<T> Search(Func<T, bool> predicate)
+    {
+        throw new NotImplementedException();
+    }
 }
