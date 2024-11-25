c1f04a7d7c697fce3ca3a1a6c7bd59cb6428c68c
Andreas-Zocom <142212797+Andreas-Zocom@users.noreply.github.com>
2024-11-25 15:03:24 +0100

    WIP - Merge Conflict

---
+++ HenriksHobbyLager/Repositories/Crud/CreateProduct.cs
@@ -0,0 +1,27 @@
+﻿namespace HenriksHobbylager.Models;
+
+private void CreateProduct()
+{
+    Console.WriteLine("Skriv in produktens namn: ");
+    var productName = Console.ReadLine();
+    System.Console.WriteLine("skriv in produkters kategori");
+    var category = Console.ReadLine();
+    Console.WriteLine("Skriv in antal: ");
+    var productQuantity = int.Parse(Console.ReadLine());
+    Console.WriteLine("Skriv in priset på produkten: ");
+    var productPrice = decimal.Parse(Console.ReadLine());
+    // _productFacade.AddProduct(productName, productQuantity, productPrice);
+
+    // TODO: A
+    var product = new Product
+    {
+
+        Name = productName,
+        Price = productPrice,
+        Stock = productQuantity,
+        Category = category,
+        Created = DateTime.Now,
+        LastUpdated = DateTime.Now
+    };
+    // var category = Console.ReadLine();
+}
--- HenriksHobbyLager/Repositories/Crud/DeleteProduct.cs
+++ HenriksHobbyLager/Repositories/Crud/DeleteProduct.cs
@@ -2,18 +2,18 @@
 
 public class DeleteProduct
 {
-    Console.WriteLine("Skriv in produkt-id: ");
+    // Console.WriteLine("Skriv in pr odukt-id: ");
     // TODO: Look this over.
     // if (!int.TryParse(Console.ReadLine(), out int id))
     // {
     //     Console.WriteLine("Ogiltigt ID! Bara siffror är tillåtna här.");
     //     return;
     // }
-    var productId = int.Parse(Console.ReadLine());
+    // var productId = int.Parse(Console.ReadLine());
     // if (product == null)
     // {
     //     Console.WriteLine("Produkt hittades inte! Puh, inget blev raderat av misstag!");
     //     return;
     // }
-    _productFacade.DeleteProduct(productId);
+    // _productFacade.DeleteProduct(productId);
 }
