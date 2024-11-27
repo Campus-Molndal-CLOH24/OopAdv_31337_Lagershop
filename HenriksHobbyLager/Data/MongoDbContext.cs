b844796aee8cd449a4da40dd34335453daecf144
Fredrik Svard <haffy@icloud.com>
2024-11-27 14:21:04 +0100

    Implemented MongoDb via Docker composer

---
+++ HenriksHobbyLager/Data/MongoDbContext.cs
@@ -0,0 +1,20 @@
+﻿using HenriksHobbylager.Models;
+using MongoDB.Driver;
+
+namespace HenriksHobbylager.Data;
+public class MongoDbContext
+{
+    public IMongoDatabase Database { get; }
+
+    public MongoDbContext(string connectionString, string databaseName)
+    {
+        if (string.IsNullOrWhiteSpace(connectionString) || string.IsNullOrWhiteSpace(databaseName))
+            throw new ArgumentException("Connection string and database name must be provided.");
+
+        var client = new MongoClient(connectionString);
+        Database = client.GetDatabase(databaseName);
+    }
+
+    public IMongoCollection<Product> Products =>
+        Database.GetCollection<Product>("Products");
+}
---
+++ HenriksHobbyLager/Data/SQLiteDbContext.cs
@@ -0,0 +1,19 @@
+﻿using HenriksHobbylager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HenriksHobbylager.Data;
+public class SQLiteDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    private readonly string _dbPath;
+
+    public SQLiteDbContext(string dbPath)
+    {
+        _dbPath = dbPath ?? throw new ArgumentNullException(nameof(dbPath));
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlite($"Data Source={_dbPath}");
+    }
+}
